756d014f7df5a40578381611231c46e514b7e365
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-10-24 11:32:23 +0530

    feat: create primary button variant style

---
+++ src/Constants.ts
@@ -0,0 +1,16 @@
+export enum ButtonVariants {
+  PRIMARY = "Primary",
+  SECONDARY = "Secondary",
+  TERTIARY = "Tertiary"
+}
+
+export enum ButtonSizes {
+    NORMAL = "Normal",
+    MEDIUM = "Medium",
+    SMALL = "Small"
+}
+
+export enum ButtonDirections {
+    LEFT = "Left",
+    RIGHT = "Right"
+}
---
+++ src/components/Button.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="button button--primary button--normal">
+    <span class="block body-1">Label</span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { withDefaults } from "vue";
+
+import { ButtonSizes, ButtonVariants } from "../Constants";
+
+type Props = {
+  variant?: string;
+  size?: string;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  variant: ButtonVariants.PRIMARY,
+  size: ButtonSizes.NORMAL,
+});
+</script>
+
+<style lang="scss" scoped>
+.button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &--primary {
+    background-color: #4caf4f;
+    color: white;
+    border-radius: 4px;
+  }
+
+  &--normal {
+    padding: 16px 32px;
+  }
+}
+</style>
