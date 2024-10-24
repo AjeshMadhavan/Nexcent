e9616ac617ce102840aa3f2e167f4db62d7d53e0
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-10-24 19:56:36 +0530

    refactor: change default styles in button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
+<Button label="Hello" class="custom-button" variant="Tertiary"/>
 </template>
 
 <script setup lang="ts">
+import Button from './components/Button.vue';
 </script>
+
+<style>
+.custom-button {
+    padding: 40px 12px;
+    font-size: 40px
+}
+</style>
