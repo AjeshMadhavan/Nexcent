8d9b6e25c1d4dbbe69dd4dbb4eff65a3c0a18c86
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-10-24 19:58:50 +0530

    refactor: clear app file

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
-<Button label="Hello" class="custom-button" variant="Tertiary"/>
 </template>
 
 <script setup lang="ts">
-import Button from './components/Button.vue';
 </script>
-
-<style>
-.custom-button {
-    padding: 40px 12px;
-    font-size: 40px
-}
-</style>
