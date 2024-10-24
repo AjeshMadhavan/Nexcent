c5efdd95d7e37030fd0fdcb67d3ab591399e60dd
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-10-24 19:01:07 +0530

    feat: add responsiveness

--- src/conatiners/HeroBanner.vue
+++ src/conatiners/HeroBanner.vue
@@ -1,22 +1,33 @@
 <template>
   <div
-    class="relative px-36 py-24 flex items-center justify-between lg:gap-26"
+    class="relative px-2 py-24 flex items-center justify-center md:px-3 xl:px-36 xl:justify-between lg:gap-26"
     style="background-color: rgba(245, 247, 250, 1)"
   >
-    <div>
-      <h1 class="font-inter text-headline-1">
-        Lessons and insights&nbsp;<span class="block text-devil-grass"
-          >from 8 years</span
-        >
+    <div class="z-10 flex flex-col">
+      <h1
+        class="font-inter text-3xl text-center sm:text-4xl md:text-6xl xl:text-left xl:text-headline-1"
+      >
+        {{ title.text }}&nbsp;
+        <span class="xl:block text-devil-grass">
+          {{ title.hightlight }}
+        </span>
       </h1>
-      <p class="text-grey mt-4">
-        Where to grow your business as a photographer: site or social media?
+      <p
+        class="mt-4 font-inter text-grey text-sm text-center md:text-base xl:text-start"
+      >
+        {{ description }}
       </p>
       <!--empty wrapper to implement button-->
       <div class="button-wrapper h-14"></div>
     </div>
-    <div>
-      <img class="block" src="../assets/Courses.svg" alt="" />
+    <div
+      class="absolute top-0 right-0 z-0 h-full opacity-10 xl:relative xl:h-fit xl:opacity-100"
+    >
+      <img
+        class="block h-full xl:h-auto"
+        src="../assets/Courses.svg"
+        alt="courses-image"
+      />
     </div>
     <div class="absolute bottom-4 left-0 flex justify-center gap-2 w-full">
       <div class="w-2.5 h-2.5 bg-devil-grass rounded-full" />
@@ -26,6 +37,10 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import uiData from "../data/uiData.json";
+
+const { title, description } = uiData.heroBanner;
+</script>
 
 <style scoped></style>
