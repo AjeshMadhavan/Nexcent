c66fd19cb97c6ae6d5ec9ce74b2598769d17a8c7
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-10-25 18:04:18 +0530

    feat: created community card

---
+++ src/containers/CommunityCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="relative w-fit font-inter">
+    <img :src="props.image" :alt="COMMUNITY_CARD_IMG_ALT" />
+    <div
+      class="bg-silver rounded-lg p-4 flex flex-col items-center gap-4 absolute bottom-2 left-6.375 right-6.375 translate-y-1/2 shadow-xl"
+    >
+      <h4 class="text-headline-4 text-grey text-center">
+        {{ props.description }}
+      </h4>
+      <Button
+        :variant="ButtonVariants.Tertiary"
+        label="Readmore"
+        :image="RightArrow"
+        class="custom-button text-headline-4 py-2"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import RightArrow from "../assets/RightArrow.svg";
+import Button from "../components/Button.vue";
+import { ButtonVariants } from "../Constants";
+
+interface Props {
+  description: string;
+  image: string;
+}
+
+const props = defineProps<Props>();
+const COMMUNITY_CARD_IMG_ALT = "community image";
+</script>
+
+<style scoped lang="scss">
+.custom-button {
+  background-color: transparent;
+}
+</style>
