9b8d9cd89fdd0c444a9e48038ec904983c3df8d6
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-10-25 10:17:20 +0530

    refactor: changed button element with custom button component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <div>
-    <header-section />
-  </div>
 </template>
 
 <script setup lang="ts">
-import HeaderSection from "./containers/HeaderSection.vue";
 </script>
--- src/containers/HeaderSection.vue
+++ src/containers/HeaderSection.vue
@@ -1,34 +1,45 @@
 <template>
   <div
-    class="flex items-center justify-between py-5.5 px-4 md:px-8 lg:px-20 xl:pr-30 xl:pl-26.25"
+    class="bg-silver flex items-center justify-between py-5.5 px-4 md:px-8 lg:px-20 xl:pr-30 xl:pl-26.25"
   >
     <img :src="Nexcent" />
     <div class="hidden md:flex md:items-center md:gap-2 lg:gap-6 xl:gap-12.5">
       <a
-        href="#"
         v-for="link in header.navbarLinks"
         :key="link"
+        href="#"
         class="text-body-2 font-medium"
       >
         {{ link }}
       </a>
     </div>
     <div class="flex items-center gap-3.5">
-      <button
-        class="p-2 md:py-2.5 md:px-5 text-brand-primary text-body-3 font-medium"
-      >
-        Login
-      </button>
-      <button
-        class="p-2 md:py-2.5 md:px-5 bg-brand-primary rounded-md text-white text-body-3 font-medium"
-      >
-        Sign up
-      </button>
+      <Button
+        :label="header.buttonTexts.login"
+        :variant="ButtonVariants.Tertiary"
+        class="p-2 md:py-2.5 md:px-5 text-body-3 font-medium w-fit login-button"
+      />
+      <Button
+        :label="header.buttonTexts.signup"
+        class="p-2 md:py-2.5 md:px-5 text-body-3 font-medium w-fit signup-button"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { header } from "../data/uiData.json";
 import Nexcent from "../assets/logo/Nexcent.svg";
+import { header } from "../data/uiData.json";
+import Button from "../components/Button.vue";
+import { ButtonVariants } from "../Constants";
 </script>
+
+<style lang="scss" scoped>
+.login-button {
+  background-color: transparent;
+}
+
+.signup-button {
+  border-radius: 6px;
+}
+</style>
